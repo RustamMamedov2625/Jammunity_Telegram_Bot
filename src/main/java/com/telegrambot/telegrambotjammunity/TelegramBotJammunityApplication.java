5f57ccc4cfabbecf8334d3726452b457588359b3
RustamMamedov2625 <mamedovrustam2509@gmail.com>
2025-08-04 00:32:55 +0400

    Project was successfully created.

---
+++ src/main/java/com/telegrambot/telegrambotjammunity/TelegramBotJammunityApplication.java
@@ -0,0 +1,13 @@
+package com.telegrambot.telegrambotjammunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotJammunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotJammunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telegrambot/telegrambotjammunity/TelegramBotJammunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegrambot.telegrambotjammunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotJammunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
