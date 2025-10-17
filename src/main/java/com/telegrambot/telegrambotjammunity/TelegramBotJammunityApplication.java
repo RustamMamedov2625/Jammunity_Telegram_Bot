07c42be4db51bd7fe26e0edfb091e7f7a4617589
RustamMamedov2625 <mamedovrustam2509@gmail.com>
2025-10-17 09:58:29 +0400

    Инициализация проекта Spring Boot с Telegram Bot API.

--- src/main/java/com/telegrambot/telegrambotjammunity/TelegramBotJammunityApplication.java
+++ src/main/java/com/telegrambot/telegrambotjammunity/TelegramBotJammunityApplication.java
@@ -3,11 +3,18 @@ package com.telegrambot.telegrambotjammunity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Главный класс приложения.
+ * Аннотация @SpringBootApplication включает в себя:
+ * - @Configuration: указывает, что класс содержит конфигурацию Spring.
+ * - @EnableAutoConfiguration: автоматически настраивает Spring Boot на основе зависимостей.
+ * - @ComponentScan: сканирует пакеты на наличие компонентов (бины), таких как @Service, @Repository, @Controller.
+ */
 @SpringBootApplication
 public class TelegramBotJammunityApplication {
 
     public static void main(String[] args) {
+        // Запускаем Spring Boot приложение.
         SpringApplication.run(TelegramBotJammunityApplication.class, args);
     }
-
 }
